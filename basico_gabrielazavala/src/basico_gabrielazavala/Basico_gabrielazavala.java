b2e5df0b0a6bd9dc4e8a8c631e29e3c64aae8d48
Gabriela Zavala <Propietario@pc-1559579848>
2020-05-07 16:29:10 +0200

    fin numericos

--- basico_gabrielazavala/src/basico_gabrielazavala/Basico_gabrielazavala.java
+++ basico_gabrielazavala/src/basico_gabrielazavala/Basico_gabrielazavala.java
@@ -11,11 +11,27 @@ package basico_gabrielazavala;
  */
 public class Basico_gabrielazavala {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+ public static void numericos() {
+
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.print("Valor entero ");
+        System.out.println(entero);
+        System.out.print("Valor doble ");
+        System.out.println(doble);    
+ }
+        
+ public static void main(String[] args) {
+      numericos();
+}
 }
